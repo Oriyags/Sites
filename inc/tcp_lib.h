7eb722b0ab9bb297c7f19cacd79b470909bf11c8
Oriya <oriyas100@gmail.com>
2024-08-18 14:11:36 +0300

    headers for funcs

--- inc/tcp_lib.h
+++ inc/tcp_lib.h
@@ -1,13 +1,13 @@
-#if !defined(SOCKET_LIB_H)
-#define SOCKET_LIB_H
+#if !defined(TCP_LIB_H)
+#define TCP_LIB_H
 
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 
-
-
-
-
+int tcp_connect(const char *host, int port);
+int tcp_send(int sockfd, const char *data, int len);
+int tcp_receive(int sockfd, char *buffer, int buffer_len);
+int tcp_close(int sockfd);
 
 #endif
