737c5e6323553ad124f42b82504bfded6822f0de
Oriya <oriyas100@gmail.com>
2024-08-18 15:02:34 +0300

    named funcs

---
+++ src/tcp_lib.c
@@ -0,0 +1,22 @@
+#include <tcp_lib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+int tcp_connect(const char *host, int port) {
+
+}
+
+int tcp_send(int sockfd, const char *data, int len) {
+
+}
+
+int tcp_receive(int sockfd, char *buffer, int buffer_len) {
+
+}
+
+int tcp_close(int sockfd) {
+    
+}
