666130387ff5e00336a2054dcda828c36da18ed5
Oriya <oriyas100@gmail.com>
2024-09-13 20:22:51 +0300

    Headers for new functions

--- library/sockets_lib.h
+++ library/sockets_lib.h
@@ -1,10 +1,32 @@
 #ifndef SOCKETS_LIB_H
 #define SOCKETS_LIB_H
 
-#include <stdint.h>
 #include <sys/types.h>
+#include <stdint.h>
+#include <stddef.h>
 
-// Socket Management Functions
+#define AF_INET 2
+#define INET_ADDRSTRLEN 16
+
+struct in_addr {
+    unsigned long s_addr;
+};
+
+struct sockaddr_in {
+    short sin_family;
+    unsigned short sin_port;
+    struct in_addr sin_addr;
+    char sin_zero[8];
+};
+
+
+void *my_memset(void *s, int c, size_t n);
+
+uint32_t my_htonl(uint32_t hostlong);
+uint16_t my_htons(uint16_t hostshort);
+uint32_t my_ntohl(uint32_t netlong);
+uint16_t my_ntohs(uint16_t netshort);
+
 int my_socket(int domain, int type, int protocol);
 int my_bind(int sockfd, const void *addr, uint32_t addrlen);
 int my_listen(int sockfd, int backlog);
@@ -12,22 +34,17 @@ int my_accept(int sockfd, void *addr, uint32_t *addrlen);
 int my_connect(int sockfd, const void *addr, uint32_t addrlen);
 int my_close(int sockfd);
 
-// Data Transmission Functions
 ssize_t my_send(int sockfd, const void *buf, size_t len, int flags);
 ssize_t my_recv(int sockfd, void *buf, size_t len, int flags);
 ssize_t my_sendto(int sockfd, const void *buf, size_t len, int flags, const void *dest_addr, uint32_t addrlen);
 ssize_t my_recvfrom(int sockfd, void *buf, size_t len, int flags, void *src_addr, uint32_t *addrlen);
 
-// Network Utility Functions
-uint32_t my_htonl(uint32_t hostlong);
-uint16_t my_htons(uint16_t hostshort);
-uint32_t my_ntohl(uint32_t netlong);
-uint16_t my_ntohs(uint16_t netshort);
 int my_inet_pton(int af, const char *src, void *dst);
 const char *my_inet_ntop(int af, const void *src, char *dst, uint32_t size);
 
-// Memory and Error Handling Functions
-void *my_memset(void *s, int c, size_t n);
-void my_perror(const char *s);
+int my_setsockopt(int sockfd, int level, int optname, const void *optval, uint32_t optlen);
+int my_getsockopt(int sockfd, int level, int optname, void *optval, uint32_t *optlen);
 
-#endif // MYSOCKET_H
+int my_fcntl(int sockfd, int cmd, long arg);
+
+#endif
