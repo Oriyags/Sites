2005c00c2bc717d36b367193e01cf2667885af3e
Oriya <oriyas100@gmail.com>
2024-10-08 16:38:08 +0300

    Fixed functions

--- test_library/client/src/tcp_client.c
+++ test_library/client/src/tcp_client.c
@@ -2,15 +2,17 @@
 #include "sockets_lib.h"
 #include <stdio.h>
 #include <string.h>
-#include <stdint.h>
 
 #define PORT 8080
 
-int tcp_connect_send_receive(const char *message) {
+// Utility function to handle TCP connection, sending, and receiving
+int tcp_client() {
     int sock = 0;
     struct my_sockaddr_in serv_addr;
     char buffer[1024] = {0};
+    const char *message = "Hello from TCP client";
 
+    // Create socket
     if ((sock = my_socket(MY_AF_INET, MY_SOCK_STREAM, 0)) < 0) {
         my_perror("Socket creation failed");
         return -1;
@@ -20,21 +22,21 @@ int tcp_connect_send_receive(const char *message) {
     serv_addr.sin_family = MY_AF_INET;
     serv_addr.sin_port = my_htons(PORT);
 
-    if (my_inet_pton(MY_AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
-        my_perror("Invalid address/ Address not supported");
-        return -1;
-    }
-
+    // Connect to server
     if (my_connect(sock, (struct my_sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
         my_perror("Connection failed");
         return -1;
     }
 
+    // Send message
     my_send(sock, message, strlen(message), 0);
-    printf("Message sent\n");
-    my_recv(sock, buffer, 1024, 0);
-    printf("Message from server: %s\n", buffer);
+    printf("TCP client: Message sent\n");
 
+    // Receive message
+    my_recv(sock, buffer, 1024, 0);
+    printf("TCP client: Message from server: %s\n", buffer);
+
+    // Close socket
     my_close(sock);
     return 0;
 }
--- test_library/client/src/udp_client.c
+++ test_library/client/src/udp_client.c
@@ -2,15 +2,17 @@
 #include "sockets_lib.h"
 #include <stdio.h>
 #include <string.h>
-#include <stdint.h>
 
 #define PORT 8080
 
-int udp_connect_send_receive(const char *message) {
+// Utility function to handle UDP sending and receiving
+int udp_client() {
     int sockfd;
     struct my_sockaddr_in servaddr;
     char buffer[1024] = {0};
+    const char *message = "Hello from UDP client";
 
+    // Create socket
     if ((sockfd = my_socket(MY_AF_INET, MY_SOCK_DGRAM, 0)) < 0) {
         my_perror("Socket creation failed");
         return -1;
@@ -19,13 +21,16 @@ int udp_connect_send_receive(const char *message) {
     my_memset(&servaddr, 0, sizeof(servaddr));
     servaddr.sin_family = MY_AF_INET;
     servaddr.sin_port = my_htons(PORT);
-    my_inet_pton(MY_AF_INET, "127.0.0.1", &servaddr.sin_addr);
 
+    // Send message
     my_sendto(sockfd, message, strlen(message), 0, (struct my_sockaddr *)&servaddr, sizeof(servaddr));
-    printf("Message sent\n");
-    my_recvfrom(sockfd, buffer, 1024, 0, NULL, NULL);
-    printf("Message from server: %s\n", buffer);
+    printf("UDP client: Message sent\n");
 
+    // Receive response
+    my_recvfrom(sockfd, buffer, 1024, 0, NULL, NULL);
+    printf("UDP client: Message from server: %s\n", buffer);
+
+    // Close socket
     my_close(sockfd);
     return 0;
 }
