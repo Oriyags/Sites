7a07ce2192049022ee0bc72150269068d1740c1c
Oriya <oriyas100@gmail.com>
2024-10-14 20:45:43 +0300

    Removed unnecessary functions

--- test_library/client/inc/udp_client.h
+++ test_library/client/inc/udp_client.h
@@ -1,12 +1,11 @@
 #if !defined(UDP_CLIENT_H)
 #define UDP_CLIENT_H
 
+#include "udp_client.h"
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 
-ssize_t my_sendto(int sockfd, const void *buf, size_t len, int flags, const void *dest_addr, uint32_t addrlen);
-ssize_t my_recvfrom(int sockfd, void *buf, size_t len, int flags, void *src_addr, uint32_t *addrlen);
 int my_close(int sockfd);
 
 #endif
