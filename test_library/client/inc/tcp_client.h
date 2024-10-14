e408cbbfe8560334edd45c0d6116f8c9b9335884
Oriya <oriyas100@gmail.com>
2024-10-14 20:46:26 +0300

    Re-write necessary functions

--- test_library/client/inc/tcp_client.h
+++ test_library/client/inc/tcp_client.h
@@ -1,10 +1,12 @@
 #if !defined(TCP_CLIENT_H)
 #define TCP_CLIENT_H
 
+#include "sockets_lib.h" 
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 
+int my_socket(int domain, int type, int protocol);
 int my_connect(int sockfd, const void *addr, uint32_t addrlen);
 ssize_t my_send(int sockfd, const void *buf, size_t len, int flags);
 ssize_t my_recv(int sockfd, void *buf, size_t len, int flags);
