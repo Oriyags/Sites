b36e543f946f791232ccccfddb2455aa5580f098
Oriya <oriyas100@gmail.com>
2024-10-15 10:54:11 +0300

    Fixed send and connect decelerations

--- library/sockets_lib.h
+++ library/sockets_lib.h
@@ -47,7 +47,7 @@ int sys_accept(int sockfd, void *addr, uint32_t *addrlen);
 int sys_connect(int sockfd, const struct my_sockaddr *addr, uint32_t addrlen);
 int sys_close(int sockfd);
 
-ssize_t my_send(int sockfd, const void *buf, size_t len, int flags);
+ssize_t sys_send(int sockfd, const void *buf, size_t len, int flags);
 ssize_t sys_recv(int sockfd, void *buf, size_t len, int flags);
 ssize_t sys_sendto(int sockfd, const void *buf, size_t len, int flags, const struct my_sockaddr *dest_addr, socklen_t addrlen);
 ssize_t sys_recvfrom(int sockfd, void *buf, size_t len, int flags, struct my_sockaddr *src_addr, socklen_t *addrlen);
